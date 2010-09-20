3d219be502d6d32095433d5365efcf6bea384929
Tim Blechmann <tim@klingt.org>
2010-09-20 13:03:40 +0200

    testsuite: fix server_test
    we should not try to run synths, since the synth type is hardcoded to sc_synth
    classes to avoid a virtual function call
    
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- testsuite/supernova/server_test.cpp
+++ testsuite/supernova/server_test.cpp
@@ -29,64 +29,51 @@ struct test_synth_prototype:
 BOOST_AUTO_TEST_CASE( server_test_1 )
 {
     rt_pool.init(1024 * 1024);
+    nova_server server(server_arguments::initialize(0, 0));
+    sc_factory->initialize(server_arguments::instance());
 
-    {
-        nova_server server(server_arguments::initialize(0, 0));
-        rt_pool.init(1024*1024);
-        sc_factory->initialize(server_arguments::instance());
+    server.synth_factory::register_prototype(new test_synth_prototype());
 
+    node_position_constraint to_root = std::make_pair(server.root_group(), insert);
 
-        server.synth_factory::register_prototype(new test_synth_prototype());
-
-        node_position_constraint to_root = std::make_pair(server.root_group(), insert);
-
-        abstract_synth * s = server.add_synth("foo", 1, to_root);
-        server.free_node(s);
-    }
-
-    instance = 0;
+    abstract_synth * s = server.add_synth("foo", 1, to_root);
+    server.free_node(s);
 }
 
 BOOST_AUTO_TEST_CASE( server_test_2 )
 {
-    {
-        nova_server server(server_arguments::initialize(0, 0));
-        server.synth_factory::register_prototype(new test_synth_prototype());
+    nova_server server(server_arguments::initialize(0, 0));
+    server.synth_factory::register_prototype(new test_synth_prototype());
 
-        node_position_constraint to_root = std::make_pair(server.root_group(), insert);
+    node_position_constraint to_root = std::make_pair(server.root_group(), insert);
 
-        abstract_synth * s = server.add_synth("foo", 1, to_root);
-        server();
-        server();
-        server();
-        server.free_node(s);
-    }
-
-    instance = 0;
+    abstract_synth * s = server.add_synth("foo", 1, to_root);
+    s->pause();
+    server();
+    server();
+    server();
+    server.free_node(s);
 }
 
 BOOST_AUTO_TEST_CASE( server_test_3 )
 {
-    {
-        nova_server server(server_arguments::initialize(0, 0));
-        server.synth_factory::register_prototype(new test_synth_prototype());
+    nova_server server(server_arguments::initialize(0, 0));
+    server.synth_factory::register_prototype(new test_synth_prototype());
 
-        parallel_group * g = new parallel_group(1);
+    parallel_group * g = new parallel_group(1);
 
-        server.add_node(g);
+    server.add_node(g);
 
-        node_position_constraint to_group = std::make_pair(g, insert);
+    node_position_constraint to_group = std::make_pair(g, insert);
 
-        abstract_synth * s1 = server.add_synth("foo", 2, to_group);
-        abstract_synth * s2 = server.add_synth("foo", 3, to_group);
-        abstract_synth * s3 = server.add_synth("foo", 4, to_group);
-        server();
-        server();
-        server();
-        server.free_node(s1);
-        server.free_node(s2);
-        server.free_node(s3);
-    }
-
-    instance = 0;
+    abstract_synth * s1 = server.add_synth("foo", 2, to_group);
+    abstract_synth * s2 = server.add_synth("foo", 3, to_group);
+    abstract_synth * s3 = server.add_synth("foo", 4, to_group);
+    g->pause();
+    server();
+    server();
+    server();
+    server.free_node(s1);
+    server.free_node(s2);
+    server.free_node(s3);
 }
