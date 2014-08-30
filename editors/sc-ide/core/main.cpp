a9e0e9fdd34f71200e856254659210ad49c92b24
Tim Blechmann <tim@klingt.org>
2014-08-30 10:43:19 +0200

    scide: introduce dark palette

--- editors/sc-ide/core/main.cpp
+++ editors/sc-ide/core/main.cpp
@@ -39,6 +39,7 @@
 #include <QDir>
 #include <QFileOpenEvent>
 #include <QLibraryInfo>
+#include <QStyleFactory>
 #include <QTranslator>
 #include <QDebug>
 
@@ -83,6 +84,8 @@ int main( int argc, char *argv[] )
     // Set up style
 
     app.setStyle( new ScIDE::Style(app.style()) );
+
+
 
     // Go...
 
@@ -217,6 +220,8 @@ Main::Main(void) :
     connect(mScProcess, SIGNAL(response(QString,QString)),
             mDocManager, SLOT(handleScLangMessage(QString,QString)));
 
+    setDarkPalette();
+
     qApp->installEventFilter(this);
     qApp->installNativeEventFilter(this);
 }
@@ -269,6 +274,33 @@ bool Main::nativeEventFilter(const QByteArray &, void * message, long *)
 #endif 
   
     return result;
+}
+
+void Main::setDarkPalette()
+{
+    qApp->setStyle(QStyleFactory::create("Fusion"));
+
+    QPalette darkPalette;
+    darkPalette.setColor(QPalette::Window,        QColor(20, 20, 20));
+    darkPalette.setColor(QPalette::WindowText,    Qt::white);
+    darkPalette.setColor(QPalette::Base,          QColor(10, 10, 10));
+    darkPalette.setColor(QPalette::AlternateBase, QColor(15, 15, 15));
+    darkPalette.setColor(QPalette::ToolTipBase,   Qt::white);
+    darkPalette.setColor(QPalette::ToolTipText,   Qt::white);
+    darkPalette.setColor(QPalette::Text,          Qt::white);
+    darkPalette.setColor(QPalette::Button,        QColor(30, 30, 30));
+    darkPalette.setColor(QPalette::ButtonText,    Qt::white);
+    darkPalette.setColor(QPalette::BrightText,    Qt::red);
+    darkPalette.setColor(QPalette::Link,          QColor(42, 130, 218));
+
+    darkPalette.setColor(QPalette::Highlight,     QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+
+    darkPalette.setColor(QPalette::Mid, QColor(27, 27, 27));
+
+    qApp->setPalette(darkPalette);
+
+    qApp->setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }");
 }
 
 
